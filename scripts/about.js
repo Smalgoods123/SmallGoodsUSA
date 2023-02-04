095e8112e92fcb167a46f0e708831b83027f1d95
ccheung8 <c4cheung@ucsd.edu>
2023-02-04 01:11:31 -0800

    finished mobile page

--- scripts/about.js
+++ scripts/about.js
@@ -7,6 +7,8 @@ const cheesemailThanks = document.querySelector("#cheesemail-thanks");
 // Hides subscribe after subbing
 signUpBtn.addEventListener('click', () => {
   cheesemailForm.classList.add("hidden");
+  // Keeps cheesemail background same height
+  cheesemailContent.style.padding = '17vh 0';
   cheesemailThanks.classList.remove("hidden");
   }
 );
