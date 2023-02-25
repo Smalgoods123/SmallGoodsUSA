6ba4660258bc4a2f3ca83f9fbcdab1739250ce05
Ziting Xiong <73570927+zitixiRose@users.noreply.github.com>
2023-02-25 13:56:50 -0800

    Fix menu, catering, about pages bugs

--- scripts/about.js
+++ scripts/about.js
@@ -23,3 +23,15 @@ window.addEventListener("load", function() {
     })
   });
 });
+
+const hamburger = document.querySelector(".hamburger");
+/*const navImg = document.querySelector(".navImg");*/
+const navHome = document.querySelector(".navHome");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    /*navImg.classList.toggle("active");*/
+    navHome.classList.toggle("active");
+}
